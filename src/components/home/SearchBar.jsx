a71d8146f3946be53fe8e62dad36c36105fa1e01
zahra noori sorkhani <zahranuori.1375@gmail.com>
2025-03-02 17:32:15 +0330

    Create and style component search

--- src/components/home/SearchBar.jsx
+++ src/components/home/SearchBar.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <div className='border-2 border-[#BCDDFE] w-[30%] flex justify-between rounded-md'>
+    <input className='pl-3 w-full outline-0' type='text' placeholder='Search query...'/>
+    <input className='bg-[#40BFFF] font-bold text-xl text-white px-5 py-3 cursor-pointer hover:bg-[#FF4858] transition' type="submit" value='Search'/>
+    </div>
   )
 }
