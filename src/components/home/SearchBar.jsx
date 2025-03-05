20fcebc1b94f28362ee725f13c03c13466b4675d
najmenoor490@gmail.com <njmeN>
2025-03-05 21:29:01 +0330

    base styles edited

--- src/components/home/SearchBar.jsx
+++ src/components/home/SearchBar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const SearchBar = () => {
   return (
-    <div className='border-2 border-[#BCDDFE] w-[30%] flex justify-between rounded-md'>
+    <div className='border-2 border-secondary w-[30%] flex justify-between rounded-md'>
     <input className='pl-3 w-full outline-0' type='text' placeholder='Search query...'/>
-    <input className='bg-[#40BFFF] font-bold text-xl text-white px-5 py-3 cursor-pointer hover:bg-[#FF4858] transition' type="submit" value='Search'/>
+    <input className='bg-primary font-bold text-xl text-white px-5 py-3 cursor-pointer hover:bg-discount transition' type="submit" value='Search'/>
     </div>
   )
 }
