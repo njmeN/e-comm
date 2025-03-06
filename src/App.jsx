f6abf90e9c9a004ba7753237bd098b18a8e45ca9
zahra noori sorkhani <zahranuori.1375@gmail.com>
2025-03-06 19:00:49 +0330

    centering & addeing container to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Home } from './components/home/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container flex flex-col items-center">
        <Header/>
        <Routes>
        <Route path="/" element={<Home />} />
